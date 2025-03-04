e4c45af34caca5df7a9fb9da2fd48de51dddf7f9
Daniil_Cherny <Dana2412dana@gmail.com>
2025-03-04 16:04:54 +0500

    added file tarrifs

---
+++ data_servers/tariffs.py
@@ -0,0 +1,26 @@
+tariffs = {
+    'trial': {
+        'days': 2,
+        'status': 'test'
+    },
+    'month': {
+        'days': 30,
+        'status': 'active'
+    },
+    'two_months': {
+        'days': 60,
+        'status': 'active'
+    },
+    'three_months': {
+        'days': 90,
+        'status': 'active'
+    },
+    'six_months': {
+        'days': 180,
+        'status': 'active'
+    },
+    'year': {
+        'days': 365,
+        'status': 'active'
+    }
+}
