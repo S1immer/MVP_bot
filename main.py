806c18768d1c523377242a4f91933c8681b6ce4b
Daniil_Cherny <Dana2412dana@gmail.com>
2025-03-25 12:57:57 +0500

    New commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from data.louder import dp, bot
 from handlers import user_menu, admin_menu
+from keyboard import user_keyboard
 
 logging.basicConfig(filename='log.txt')
 
@@ -10,9 +11,9 @@ async def main():
     await user_menu.set_commands()
     dp.include_router(user_menu.router)
     dp.include_router(admin_menu.router)
+    dp.include_router(user_keyboard.router)
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
