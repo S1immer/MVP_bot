baf51a78469741864e2b5df4f4c1c78b02955a6a
daniilcernavskij <dana2412dana@gmail.com>
2025-08-16 23:32:28 +0500

    edited name

--- keyboard/user_keyboard.py
+++ keyboard/user_keyboard.py
@@ -615,7 +615,7 @@ async def active_choose_devices(callback: CallbackQuery, state: FSMContext):
         await save_ip_limit(telegram_id, selected_limit_ip_int)
         await callback.message.delete()
         await callback.message.answer(
-            f"✅ Количество устройств уменьшено с {current_user_limit_ip} до {selected_devices}.\n"
+            f"✅ Количество устройств уменьшено с {current_user_limit_ip} до {selected_limit_ip_int}.\n"
             f"📅 Подписка действует до {deleted_at.date()}."
         )
         await state.clear()
