1833dc73be426b3006c84eb0c2f6d5f6b36e23ee
daniilcernavskij <dana2412dana@gmail.com>
2025-08-19 20:45:32 +0500

    updated parametr for function

--- payment/yookassa/yookassa_function.py
+++ payment/yookassa/yookassa_function.py
@@ -3,7 +3,7 @@ from yookassa import Payment
 from payment.yookassa.config_yoo import RETURN_URL
 
 
-async def create_payment(user_id, tariff_date, price, quantity_devices):
+async def create_payment(user_id, tariff_days, price, quantity_devices):
     payment_id = str(uuid4())
 
     payment = Payment.create({
@@ -16,10 +16,10 @@ async def create_payment(user_id, tariff_date, price, quantity_devices):
             "return_url": RETURN_URL
         },
         "capture": True,
-        "description": f"Консультация сроком на {tariff_date} дней",
+        "description": f"Консультация сроком на {tariff_days} дней",
         "metadata": {
             "user_id": user_id,
-            "tariff_date": tariff_date,
+            "tariff_date": tariff_days,
             "quantity_devices": quantity_devices,
             "payment_id": payment_id
         },
@@ -29,7 +29,7 @@ async def create_payment(user_id, tariff_date, price, quantity_devices):
             },
             "items": [
                 {
-                    "description": f"Консультация сроком на {tariff_date} дней",
+                    "description": f"Консультация сроком на {tariff_days} дней",
                     "quantity": 1,
                     "amount": {
                         "value": f"{price}.00",
@@ -49,38 +49,8 @@ async def create_payment(user_id, tariff_date, price, quantity_devices):
 async def check_payment_status(payment_id):
     try:
         payment = Payment.find_one(payment_id)  # Получаем объект платежа
-        # print(f"Полный ответ платежной системы: {payment}")  # Логируем весь объект
-        return payment['status']  # Возвращаем только статус ('pending', 'succeeded' и т.д.)
+        # print(f"Полный ответ платежной системы: {payment.status}") # Логируем весь объект
+        return payment.status  # Возвращаем только статус ('pending', 'succeeded' и т.д.)
     except Exception as e:
         print(f"Ошибка при проверке статуса платежа: {e}")
         return None
-
-
-
-
-
-
-# async def check_payment_status(payment_id):
-#     payment = Payment.find_one(payment_id)
-#
-#     if payment.status == 'succeeded':
-#         gb_limit = payment.metadata.get('gb_limit')
-#
-#         if gb_limit is None:
-#             raise ValueError("Лимит данных не найден в метаданных платежа.")
-#
-#         try:
-#             gb_limit = int(gb_limit)
-#         except ValueError:
-#             raise ValueError(f"Невозможно преобразовать лимит данных в целое число: {gb_limit}")
-#
-#         months = data_limit_to_months.get(gb_limit)
-#         if months is None:
-#             raise ValueError(f"Неизвестный лимит данных: {gb_limit}")
-#
-#         return True, gb_limit, months
-#
-#     elif payment.status == 'canceled':
-#         return False, None, None
-
-    # return False, None, None
