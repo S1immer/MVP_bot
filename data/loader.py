a8447fc2a603fa3f50a67f35ba8e8b68782b0560
daniilcernavskij <dana2412dana@gmail.com>
2025-08-20 11:07:32 +0500

    added storage

--- data/loader.py
+++ data/loader.py
@@ -1,7 +1,10 @@
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
 from data.config import token  # Импортируем токен из config.py
 
+storage = MemoryStorage()
 bot = Bot(token)
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot, storage=storage)
 
 
