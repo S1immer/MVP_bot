fd850c56b68e91181fa555584947ddc80f7233f1
daniilcernavskij <dana2412dana@gmail.com>
2025-08-22 19:16:56 +0500

    added comment for function

--- payment/yookassa/yookassa_function.py
+++ payment/yookassa/yookassa_function.py
@@ -1,55 +1,62 @@
 from uuid import uuid4
 from yookassa import Payment
 from payment.yookassa.config_yoo import RETURN_URL
+from logs.logging_config import logger
 
 
 async def create_payment(user_id, tariff_days, price, quantity_devices):
+    """
+    :param user_id: telegram_id пользователя
+    :param tariff_days: количество дней int, для описания платежа
+    :param price: цена int
+    :param quantity_devices: количество устройств для описания платежа
+    :return:
+    """
     payment_id = str(uuid4())
-
-    payment = Payment.create({
-        "amount": {
-            "value": f"{price}.00",
-            "currency": "RUB"
-        },
-        "confirmation": {
-            "type": "redirect",
-            "return_url": RETURN_URL
-        },
-        "capture": True,
-        "description": f"Консультация сроком на {tariff_days} дней",
-        "metadata": {
-            "user_id": user_id,
-            "tariff_date": tariff_days,
-            "quantity_devices": quantity_devices,
-            "payment_id": payment_id
-        },
-        "receipt": {
-            "customer": {
-                "email": 'dan2412dan@mail.ru'
+    try:
+        payment = Payment.create({
+            "amount": {
+                "value": f"{price}.00",
+                "currency": "RUB"
             },
-            "items": [
-                {
-                    "description": f"Консультация сроком на {tariff_days} дней",
-                    "quantity": 1,
-                    "amount": {
-                        "value": f"{price}.00",
-                        "currency": "RUB"
-                    },
-                    "vat_code": 1
-                }
-            ]
-        }
-    })
-
-    # print(f"Платеж успешно создан. confirmation_url={payment.confirmation.confirmation_url}, id={payment.id}")
-
-    return payment.confirmation.confirmation_url, payment.id
+            "confirmation": {
+                "type": "redirect",
+                "return_url": RETURN_URL
+            },
+            "capture": True,
+            "description": f"Консультация сроком на {tariff_days} дней",
+            "metadata": {
+                "user_id": user_id,
+                "tariff_date": tariff_days,
+                "quantity_devices": quantity_devices,
+                "payment_id": payment_id
+            },
+            "receipt": {
+                "customer": {
+                    "email": 'dan2412dan@mail.ru'
+                },
+                "items": [
+                    {
+                        "description": f"Консультация сроком на {tariff_days} дней",
+                        "quantity": 1,
+                        "amount": {
+                            "value": f"{price}.00",
+                            "currency": "RUB"
+                        },
+                        "vat_code": 1
+                    }
+                ]
+            }
+        })
+        return payment.confirmation.confirmation_url, payment.id
+    except Exception as e:
+        logger.error(f"Ошибка создания платежа YooKassa: {e}")
+        return None, None
 
 
 async def check_payment_status(payment_id):
     try:
         payment = Payment.find_one(payment_id)  # Получаем объект платежа
-        # print(f"Полный ответ платежной системы: {payment.status}") # Логируем весь объект
         return payment.status  # Возвращаем только статус ('pending', 'succeeded' и т.д.)
     except Exception as e:
         print(f"Ошибка при проверке статуса платежа: {e}")
