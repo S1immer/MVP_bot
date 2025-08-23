201b3878b87ee17856f3ca95d4587d8a38f152a0
daniilcernavskij <dana2412dana@gmail.com>
2025-08-23 18:55:12 +0500

    added import and button for extend_sub_user

--- handlers/admin_menu/main_admin_menu.py
+++ handlers/admin_menu/main_admin_menu.py
@@ -1,12 +1,16 @@
 from aiogram import Router
 from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.filters import Command
-from handlers.admin_menu import clear_state_user
+from handlers.admin_menu.add_new_servers import router as add_new_servers_router
+from handlers.admin_menu.clear_state_user import router as clear_state_router
+from handlers.admin_menu.extend_sub_user import router as extend_sub_router
 from data.config import admins  # список админов
 
 admin_router = Router()
-admin_router.include_router(clear_state_user.router)
 
+admin_router.include_router(clear_state_router)
+admin_router.include_router(add_new_servers_router)
+admin_router.include_router(extend_sub_router)
 
 @admin_router.message(Command("admin"))
 async def admin_menu(message: Message):
@@ -17,7 +21,7 @@ async def admin_menu(message: Message):
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="🧹 Очистить состояние", callback_data="clear_state")],
-        [InlineKeyboardButton(text="📅 Продлить срок подписки", callback_data="admin_extend_sub")],
-        # Добавляйте другие кнопки по аналогии
+        [InlineKeyboardButton(text="📅 Продлить подписку", callback_data="admin_extend_sub")],
+        [InlineKeyboardButton(text="🖥️ Добавить новые сервера", callback_data="add_new_servers")],
     ])
     await message.answer(text="Админ-панель:", reply_markup=keyboard)
